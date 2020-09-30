f20df78ab36cc03b1e6fa27851791cdee8a9db1b
Alex Zaslavskis <sahsariga111@gmail.com>
2020-09-30 22:57:03 +0100

    Files

---
+++ docs.js
@@ -0,0 +1,21 @@
+	function go_frame() {
+		window.open("https://alex5250.github.io/ElectricsEaglesWebsiteDrone/docs/frame.pdf");
+	}
+
+	function go_pcb() {
+window.open("https://alex5250.github.io/ElectricsEaglesWebsiteDrone/docs/pcb_software.pdf");
+	}
+	function software() {
+window.open("https://alex5250.github.io/ElectricsEaglesWebsiteDrone/docs/software.pdf");
+	}
+	function ymfc_al() {
+window.open("http://www.brokking.net/ymfc-al_main.html");
+	}
+
+	function PlayList() {
+window.open("https://www.youtube.com/watch?v=2pHdO8m6T7c&feature=emb_logo");
+	}
+
+	function GitHub() {
+window.open("https://github.com/alex5250/Electric-Eagles");
+	}
