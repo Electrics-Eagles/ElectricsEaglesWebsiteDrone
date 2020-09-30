26c954a39d9a96e9a443e2ad4f85f7db4486d372
Alex Zaslavskis <sahsariga111@gmail.com>
2020-09-30 23:01:52 +0100

    Update windows_workers.js

--- windows_workers.js
+++ windows_workers.js
@@ -4,9 +4,11 @@
 
 
 function download() {
+    var downland_docs="https://alex5250.github.io/ElectricsEaglesWebsiteDrone/downland.html";
     window.open(downland_docs, "Docs", "width=300,height=500");
 }
 
 function docs() {
+    var link_docs="https://alex5250.github.io/ElectricsEaglesWebsiteDrone/docs.html";
     window.open(link_docs, "Docs", "width=300,height=500");
 }
