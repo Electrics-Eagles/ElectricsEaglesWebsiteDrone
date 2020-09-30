f100fed7e5820fa0c5c0ab8fa987c118504040ba
Alex Zaslavskis <sahsariga111@gmail.com>
2020-09-30 23:12:08 +0100

    Update windows_workers.js

--- windows_workers.js
+++ windows_workers.js
@@ -1,11 +1,11 @@
  var link_docs="https://alex5250.github.io/ElectricsEaglesWebsiteDrone/docs.html";
- var downland_docs="https://alex5250.github.io/ElectricsEaglesWebsiteDrone/downland.html";
+ var downland_docs="https://alex5250.github.io/ElectricsEaglesWebsiteDrone/downlands.html";
 
 
 
 function download_data() {
     var link_docs="https://alex5250.github.io/ElectricsEaglesWebsiteDrone/docs.html";
- var downland_docs="https://alex5250.github.io/ElectricsEaglesWebsiteDrone/downland.html";
+ var downland_docs="https://alex5250.github.io/ElectricsEaglesWebsiteDrone/downlands.html";
 
  window.open(downland_docs, "Docs", "width=300,height=500");
  
@@ -13,7 +13,7 @@ function download_data() {
 
 function docs() {
      var link_docs="https://alex5250.github.io/ElectricsEaglesWebsiteDrone/docs.html";
- var downland_docs="https://alex5250.github.io/ElectricsEaglesWebsiteDrone/downland.html";
+ var downland_docs="https://alex5250.github.io/ElectricsEaglesWebsiteDrone/downlands.html";
 
  window.open(link_docs, "Docs", "width=300,height=500");
  
