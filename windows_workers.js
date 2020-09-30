0ead272f5e40bb6c0a18241c1e1aa73afd8e300f
Alex Zaslavskis <sahsariga111@gmail.com>
2020-09-30 22:57:32 +0100

    Wale

---
+++ windows_workers.js
@@ -0,0 +1,12 @@
+ var link_docs="https://alex5250.github.io/ElectricsEaglesWebsiteDrone/docs.html";
+ var downland_docs="https://alex5250.github.io/ElectricsEaglesWebsiteDrone/downland.html";
+
+
+
+function download() {
+    window.open(downland_docs, "Docs", "width=300,height=500");
+}
+
+function docs() {
+    window.open(link_docs, "Docs", "width=300,height=500");
+}
